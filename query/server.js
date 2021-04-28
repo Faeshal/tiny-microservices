3ea29785f8269b4309dd1aa371fd4ff1cef7007a
faeshal_ <faeshal@deeeplabs.id>
2021-04-28 17:28:15 +0700

    add moderation servide

--- query/server.js
+++ query/server.js
@@ -12,19 +12,27 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-  if (type === "PostCreated") {
-    const { id, title } = data;
-    posts[id] = { id, title, comments: [] };
+app.post("/events", (req, res, next) => {
+  try {
+    const { type, data } = req.body;
+
+    if (type === "PostCreated") {
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+    }
+
+    if (type === "CommentCreated") {
+      const { id, content, postId } = data;
+      const post = posts[postId];
+
+      post.comments.push({ id, content });
+    }
+
+    console.log(posts);
+    res.send({});
+  } catch (err) {
+    next(err);
   }
-  if (type === "CommmentCreated") {
-    const { id, content, postId } = data;
-    const post = posts[postId];
-    post.comments.push({ id, content });
-  }
-  console.log(posts);
-  res.send({});
 });
 
 app.listen(4002, () => {
